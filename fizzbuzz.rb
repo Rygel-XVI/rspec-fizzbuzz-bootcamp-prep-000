719c4968dc755eee2c2ed49df0a3fcc7a218abdd
Annette Michaels <micha232@umn.edu>
2017-10-31 08:59:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,12 @@
-def fizzbuzz
-
+def fizzbuzz(x)
+  three = !!x/3
+  five = !!x/5
+  if three && five
+    puts "fizzbuzz"
+  elseif three
+    puts "fizz"
+  elseif five
+    puts "buzz"
+  else return nil
+  end
 end
