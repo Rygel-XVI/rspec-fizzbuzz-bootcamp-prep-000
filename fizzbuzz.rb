7eec36754d830c222fe629b2ab3a5c79f503383d
Annette Michaels <micha232@umn.edu>
2017-10-31 09:15:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(x)
   x.to_i
   puts "#{x/3}"
-  three = (x/3 === Fixnum)
-  five = (x/5 === Fixnum)
+  three = x%3 
+  five = x%5 
   puts "three #{three} five #{five}"
   if three && five
     return "FizzBuzz"
